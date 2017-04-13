ab5c157a77e5d835c37bfc6dd71b3eaf19166d11
10100programer <alexmettler@hotmail.com>
2017-04-13 12:33:49 -0500

    INIT

---
+++ Lab-4--Binary-Search-Tree/Lab-4--Binary-Search-Tree/Lab-4--Binary-Search-Tree.cpp
@@ -0,0 +1,13 @@
+// Lab-4--Binary-Search-Tree.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Comsci.h"
+
+int main()
+{
+	splash("Lab 4 Binary Search Tree", "In this lab, you review the terminology and theory underlying binary trees.");
+
+    return 0;
+}
+
